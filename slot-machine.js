67d8d8d908676dc7dd8b66777463c32e1c15fd3a
KrasimirDimitrov354 <krasidimitrov354@gmail.com>
2023-09-24 23:21:30 +0300

    Update slot-machine.js #3
    Added a function that checks whether or not there are any winning rows, and then calculates the winnings.

--- slot-machine.js
+++ slot-machine.js
@@ -21,53 +21,42 @@ const SYMBOLS_MULTIPLIERS = {
 };
 
 const deposit = () => {
-    while (true)
-    {
+    while (true) {
         const amountRaw = prompt("Enter deposit amount: ");
         const amountConverted = parseFloat(amountRaw);
 
-        if(isNaN(amountConverted) || amountConverted <= 0)
-        {
+        if(isNaN(amountConverted) || amountConverted <= 0) {
             console.log("You must enter a positive number value.");
-        }
-        else
-        {
+        } else {
             return amountConverted;
         }
     }
 };
 
 const getNumberOfLines = () => {
-    while (true)
-    {
+    while (true) {
         const linesRaw = prompt("Enter number of lines you want to bet on: ");
         const linesConverted = parseInt(linesRaw);
 
         if (isNaN(linesConverted) || 
-        linesConverted < MIN_LINES || linesConverted > MAX_LINES)
-        {
+        linesConverted < MIN_LINES || linesConverted > MAX_LINES) {
             console.log("You must enter a number value between 1 and 3.");
-        }
-        else
-        {
+        } else {
             return linesConverted;
         }
     }
 };
 
-const getBet = (balance, bettingLinesCount) => {
-    while (true)
-    {
+const getBet = (balance, bettingLines) => {
+    while (true) {
         const amountRaw = prompt("Enter amount for bet per line: ");
         const amountConverted = parseFloat(amountRaw);
 
         if (isNaN(amountConverted) || amountConverted <= 0 || 
-        amountConverted > balance / bettingLinesCount)
-        {
-            console.log("You must enter a positive number value no greater than " + (Math.floor(balance / bettingLinesCount)) + ".");
-        }
-        else
-        {
+        amountConverted > balance / bettingLines) {
+            console.log("You must enter a positive number value no greater than " + 
+            (Math.floor(balance / bettingLines)) + ".");
+        } else {
             return amountConverted;
         }
     }
@@ -75,22 +64,18 @@ const getBet = (balance, bettingLinesCount) => {
 
 const spin = () => {
     const symbolsTotal = [];
-    for (const [symbol, occurence] of Object.entries(SYMBOLS_OCCURENCES))
-    {
-        for (let i = 0; i < occurence; i++)
-        {
+    for (const [symbol, occurence] of Object.entries(SYMBOLS_OCCURENCES)) {
+        for (let i = 0; i < occurence; i++) {
             symbolsTotal.push(symbol);
         }
     }
 
     const reels = [];
-    for (let i = 0; i < COLS; i++)
-    {
+    for (let i = 0; i < COLS; i++) {
         reels.push([]);
         const reelSymbols = [...symbolsTotal];
         
-        for (let j = 0; j < ROWS; j++)
-        {
+        for (let j = 0; j < ROWS; j++) {
             const symbolIndex = Math.floor(Math.random() * reelSymbols.length);
             const selectedSymbol = reelSymbols[symbolIndex];
 
@@ -105,12 +90,10 @@ const spin = () => {
 const transpose = (reels) => {
     const transposedRows = [];
 
-    for (let i = 0; i < ROWS; i++)
-    {
+    for (let i = 0; i < ROWS; i++) {
         transposedRows.push([]);
 
-        for (let j = 0; j < COLS; j++)
-        {
+        for (let j = 0; j < COLS; j++) {
             transposedRows[i].push(reels[j][i]);
         }
     }
@@ -118,17 +101,14 @@ const transpose = (reels) => {
     return transposedRows;
 };
 
-const printSpin = (spin) => {
-    for (const row of spin)
-    {
+const printSpin = (spinArray) => {
+    for (const row of spinArray) {
         let rowString = "";
 
-        for (let i = 0; i < row.length; i++)
-        {
+        for (let i = 0; i < row.length; i++) {
             rowString += row[i];
 
-            if (i != row.length - 1)
-            {
+            if (i != row.length - 1) {
                 rowString += " | ";    
             }
         }
@@ -137,11 +117,54 @@ const printSpin = (spin) => {
     }
 };
 
+const getWinnings = (spinArray, betAmount, bettingLines) => {
+    let winnings = 0;
+    let winningRows = 0;
+
+    for (let i = 0; i < spinArray.length; i++) {
+        const currentRow = spinArray[i];
+        let isWinning = true;
+
+        for (const symbol of currentRow) {
+            if (symbol != currentRow[0]) {
+                isWinning = false;
+                break;
+            }
+        }
+
+        if (isWinning) {
+            winningRows++;
+
+            if (winningRows <= bettingLines) {
+                winnings += betAmount * SYMBOLS_MULTIPLIERS[currentRow[0]];
+            }
+        }
+    }
+
+    if (winningRows > bettingLines) {
+        winnings += betAmount;
+    } else if (winningRows < bettingLines && winningRows != 0) {
+        winnings -= betAmount * 1.5;
+    } else if (winningRows == 0) {
+        winnings -= betAmount * bettingLines;
+    }
+
+    if (winnings >= 0) {
+        console.log("You won!")
+    } else {
+        console.log("You lost!")
+    }
+
+    return winnings;
+};
+
 let balance = deposit();
-const bettingLinesCount = getNumberOfLines();
-const bet = getBet(balance, bettingLinesCount);
+const bettingLines = getNumberOfLines();
+const bet = getBet(balance, bettingLines);
 
 const reels = spin();
 const transposed = transpose(reels);
 
 printSpin(transposed);
+
+const winnings = getWinnings(transposed, bet, bettingLines);
