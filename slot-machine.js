c97cb817d64ed74624e5ba8d530fcff85846f108
KrasimirDimitrov354 <krasidimitrov354@gmail.com>
2023-09-24 20:06:08 +0300

    Update slot-machine.js
    Added constants and the spin() function, fixed spacing.

--- slot-machine.js
+++ slot-machine.js
@@ -1,47 +1,109 @@
 const prompt = require("prompt-sync")();
 
+const MIN_LINES = 1;
+const MAX_LINES = 3;
+
+const ROWS = 3;
+const COLS = 3;
+
+const SYMBOLS_OCCURENCES = {
+    '$': 4,
+    '\&': 8,
+    '@': 16,
+    '#': 24
+};
+
+const SYMBOLS_MULTIPLIERS = {
+    '$': 10,
+    '\&': 6,
+    '@': 5,
+    '#': 3
+};
+
 const deposit = () => {
-    while (true){
+    while (true)
+    {
         const amountRaw = prompt("Enter deposit amount: ");
         const amountConverted = parseFloat(amountRaw);
 
-        if(isNaN(amountConverted) || amountConverted <= 0){
+        if(isNaN(amountConverted) || amountConverted <= 0)
+        {
             console.log("You must enter a positive number value.");
-        } else {
+        }
+        else
+        {
             return amountConverted;
         }
     }
 };
 
-const MIN_LINES = 1;
-const MAX_LINES = 3;
-
 const getNumberOfLines = () => {
-    while (true) {
+    while (true)
+    {
         const linesRaw = prompt("Enter number of lines you want to bet on: ");
         const linesConverted = parseInt(linesRaw);
 
-        if (isNaN(linesConverted) || linesConverted < MIN_LINES || linesConverted > MAX_LINES) {
+        if (isNaN(linesConverted) || 
+        linesConverted < MIN_LINES || linesConverted > MAX_LINES)
+        {
             console.log("You must enter a number value between 1 and 3.");
-        } else {
+        }
+        else
+        {
             return linesConverted;
         }
     }
 };
 
 const getBet = (balance, bettingLinesCount) => {
-    while (true) {
+    while (true)
+    {
         const amountRaw = prompt("Enter amount for bet per line: ");
         const amountConverted = parseFloat(amountRaw);
 
-        if (isNaN(amountConverted) || amountConverted <= 0 || amountConverted > balance / bettingLinesCount) {
+        if (isNaN(amountConverted) || amountConverted <= 0 || 
+        amountConverted > balance / bettingLinesCount)
+        {
             console.log("You must enter a positive number value no greater than " + (Math.floor(balance / bettingLinesCount)) + ".");
-        } else {
+        }
+        else
+        {
             return amountConverted;
         }
     }
 };
 
+const spin = () => {
+    const symbols = [];
+    for (const [symbol, occurence] of Object.entries(SYMBOLS_OCCURENCES))
+    {
+        for (let i = 0; i < occurence; i++)
+        {
+            symbols.push(symbol);
+        }
+    }
+
+    const reels = [];
+    for (let i = 0; i < COLS; i++)
+    {
+        reels.push([]);
+        const reelSymbols = [...symbols];
+        
+        for (let j = 0; j < ROWS; j++)
+        {
+            const symbolIndex = Math.floor(Math.random() * reelSymbols.length);
+            const selectedSymbol = reelSymbols[symbolIndex];
+
+            reels[i].push(selectedSymbol);
+            reelSymbols.splice(symbolIndex, 1);
+        }
+    }
+
+    return reels;
+};
+
 let balance = deposit();
 const bettingLinesCount = getNumberOfLines();
 const bet = getBet(balance, bettingLinesCount);
+
+const reels = spin();
